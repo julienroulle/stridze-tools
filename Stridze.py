9b0625d4873a49db42e7b9edf394a69f16c74289
Julien Roullé <ju.roulle@gmail.com>
2023-05-23 17:08:19 +0200

    feat: add metric/imperial support

--- Stridze.py
+++ Stridze.py
@@ -5,11 +5,16 @@ st.set_page_config(
     page_icon="👋",
 )
 
+system = st.sidebar.radio("Unit System", ('Metric', 'Imperial'), index=0)
+if system is not None:
+    st.session_state['system'] = system
+
 st.write("# Welcome to Stridze Tools!")\
 
 st.markdown(
     """
     Stridze Tools is an open-source app built specifically for Runners.
+
     **👈 Select a tool from the sidebar** to see some examples of what Stridze can do!
 """
 )
--- pages/1_Race Pace.py
+++ pages/1_Race Pace.py
@@ -1,10 +1,10 @@
 import streamlit as st
 
 DISTANCES = {
-    "5km": 5.000,
-    "10km": 10.000,
-    "Half Marathon": 21.097,
     "Marathon": 42.195,
+    "Half Marathon": 21.097,
+    "10km": 10.000,
+    "5km": 5.000,
     "Custom": None,
 }
 
@@ -16,23 +16,27 @@ def get_pace(dist, time):
 
 st.title('Race Calculator')
 
+system = st.sidebar.radio("Unit System", ('Metric', 'Imperial'), index=0)
+if system is not None:
+    st.session_state['system'] = system
+
 pace_tab, distance_tab, time_tab = st.tabs(['Pace', 'Distance', 'Time'])
 
 with pace_tab:
+    dist_unit = 'km'
+    if st.session_state['system'] == 'Imperial':
+        dist_unit = 'mi'
     pace_tab_distance_col, _, pace_tab_time_col= st.columns([8, 0.5, 10])
 
-    pace_tab_distance_col.subheader("Distance")
+    pace_tab_distance_col.subheader(f"Distance ({dist_unit})")
 
     default_distance = pace_tab_distance_col.selectbox("Select distance", list(DISTANCES.keys()), key="pace_tab_distance_col_default_distance")
     distance = DISTANCES[default_distance]
-    pace_tab_distance_col1, pace_tab_distance_col2 = pace_tab_distance_col.columns([2, 1])
-    raw_distance = pace_tab_distance_col1.number_input("Write your distance here", 1.0, value=distance, key="pace_tab_distance_col_distance")
-    distance = raw_distance
-    dist_unit = pace_tab_distance_col2.radio("Unit", ('m', 'km', 'mi'), index=1, key="pace_tab_distance_col_unit")
-    if dist_unit == 'Kilometers (km)':
-        distance *= 1000.
-    elif dist_unit == 'Miles (mi)':
-        distance *= 1609.34
+
+    if st.session_state['system'] == 'Imperial':
+        distance /= 1.609
+
+    raw_distance = pace_tab_distance_col.number_input("Write your distance here", 1.0, value=distance, key="pace_tab_distance_col_distance")
 
     pace_tab_time_col.subheader("Time")
     pace_tab_time_col1, pace_tab_time_col2, pace_tab_time_col3 = pace_tab_time_col.columns(3)
@@ -40,11 +44,15 @@ with pace_tab:
     m = pace_tab_time_col2.number_input("Minutes", value=0, min_value=0, max_value=59, key="pace_tab_time_col_minutes")
     s = pace_tab_time_col3.number_input("Seconds", value=0, min_value=0, max_value=59, key="pace_tab_time_col_seconds")
     time = h * 3600 + m * 60 + s
-    pace_min, pace_sec = get_pace(distance, time)
+    pace_min, pace_sec = get_pace(raw_distance, time)
     st.header(f"Average pace: {pace_min:.0f}m{pace_sec:2.0f}s/{dist_unit}")
 
 
 with distance_tab:
+    dist_unit = 'km'
+    if st.session_state['system'] == 'Imperial':
+        dist_unit = 'mi'
+
     distance_tab_time_col, _, distance_tab_pace_col= st.columns([10, 0.5, 8])
 
     distance_tab_time_col.subheader("Time")
@@ -54,7 +62,7 @@ with distance_tab:
     s = distance_tab_time_col3.number_input("Seconds", value=0, min_value=0, max_value=59, key="distance_tab_time_col_seconds")
     time = h * 3600 + m * 60 + s
 
-    distance_tab_pace_col.subheader("Pace")
+    distance_tab_pace_col.subheader(f"Pace (min/{dist_unit})")
 
     distance_tab_pace_col1, distance_tab_pace_col2 = distance_tab_pace_col.columns(2)
     pace_min = distance_tab_pace_col1.number_input("Minutes", value=0, min_value=0, max_value=59, key="distance_tab_pace_col_minutes")
@@ -66,33 +74,34 @@ with distance_tab:
     except ZeroDivisionError:
         distance = 0.
 
-    st.header(f"Distance: {distance:.2f}km")
+    st.header(f"Distance: {distance:.2f} {dist_unit}")
 
 
 with time_tab:
+    dist_unit = 'km'
+    if st.session_state['system'] == 'Imperial':
+        dist_unit = 'mi'
+
     time_tab_pace_col, _, time_tab_distance_col= st.columns([8, 0.5, 8])
 
-    time_tab_pace_col.subheader("Pace")
+    time_tab_pace_col.subheader(f"Pace (min/{dist_unit})")
 
     column1, column2 = time_tab_pace_col.columns(2)
     pace_min = column1.number_input("Minutes", 0)
     pace_sec = column2.number_input("Seconds", 0)
     pace = pace_min * 60 + pace_sec
 
-    time_tab_distance_col.subheader("Distance")
+    time_tab_distance_col.subheader(f"Distance ({dist_unit})")
 
     default_distance = time_tab_distance_col.selectbox("Select distance", list(DISTANCES.keys()))
     distance = DISTANCES[default_distance]
-    column1, column2 = time_tab_distance_col.columns([2, 1])
-    raw_distance = column1.number_input("Write your distance here", 1.0, value=distance)
-    distance = raw_distance
-    dist_unit = column2.radio("Unit", ('m', 'km', 'mi'), index=1)
-    if dist_unit == 'Kilometers (km)':
-        distance *= 1000.
-    elif dist_unit == 'Miles (mi)':
-        distance *= 1609.34
 
-    time = distance * pace
+    if st.session_state['system'] == 'Imperial':
+        distance /= 1.609
+
+    raw_distance = time_tab_distance_col.number_input("Write your distance here", 1.0, value=distance)
+
+    time = raw_distance * pace
     h = time // 3600
     m = (time - h * 3600) // 60
     s = time % 60
