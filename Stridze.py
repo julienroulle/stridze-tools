0714d306fbea18cbd058e46c9a51e93e2f94b8fd
Julien Roullé <ju.roulle@gmail.com>
2023-05-23 15:58:55 +0200

    feat: Home page

--- Stridze.py
+++ Stridze.py
@@ -5,22 +5,11 @@ st.set_page_config(
     page_icon="👋",
 )
 
-st.write("# Welcome to Stridze!")\
+st.write("# Welcome to Stridze Tools!")\
 
 st.markdown(
     """
-    Streamlit is an open-source app framework built specifically for
-    Machine Learning and Data Science projects.
-    **👈 Select a demo from the sidebar** to see some examples
-    of what Streamlit can do!
-    ### Want to learn more?
-    - Check out [streamlit.io](https://streamlit.io)
-    - Jump into our [documentation](https://docs.streamlit.io)
-    - Ask a question in our [community
-        forums](https://discuss.streamlit.io)
-    ### See more complex demos
-    - Use a neural net to [analyze the Udacity Self-driving Car Image
-        Dataset](https://github.com/streamlit/demo-self-driving)
-    - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
+    Stridze Tools is an open-source app built specifically for Runners.
+    **👈 Select a tool from the sidebar** to see some examples of what Stridze can do!
 """
 )
