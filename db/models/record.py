ed9a2243982c9e73a04f66eb5545bc53899281db
Julien Roullé <ju.roulle@gmail.com>
2023-07-03 10:23:03 +0000

    feat: new record db

---
+++ db/models/record.py
@@ -0,0 +1,25 @@
+from sqlalchemy import BIGINT, Column, Integer, Float, ForeignKey
+from sqlalchemy.orm import relationship
+
+from .. import Base
+
+class Record(Base):
+    __tablename__ = 'records'
+
+    id = Column(Integer, primary_key=True)
+    timestamp = Column(Float)
+    position_lat = Column(Float)
+    position_long = Column(Float)
+    distance = Column(Float)
+    altitude = Column(Float)
+    enhanced_altitude = Column(Float)
+    speed = Column(Float)
+    enhanced_speed = Column(Float)
+    heart_rate = Column(Integer)
+    cadence = Column(Integer)
+    activity_id = Column(BIGINT, ForeignKey('activities.id'))
+
+    activity = relationship('Activity', back_populates='records')
+
+    def __repr__(self):
+        return f"<Record(id={self.id}, lap_id={self.lap_id})>"
