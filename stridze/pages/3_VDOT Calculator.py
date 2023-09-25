5aa0ebed8ceb8a78a3ead6a6b20710ce8594022c
Julien Roullé <ju.roulle@gmail.com>
2023-09-25 15:00:42 +0200

    fix: data imports and comment out calendar

--- stridze/pages/3_VDOT Calculator.py
+++ stridze/pages/3_VDOT Calculator.py
@@ -1,17 +1,16 @@
+import pandas as pd
 import streamlit as st
 
-import pandas as pd
-
-from src.vdot import user_VDOT
-from src.utils import num_time_to_str_time
+from stridze.utils import num_time_to_str_time
+from stridze.vdot import user_VDOT
 
 DISTANCES = {
     "Marathon": 42.195,
     "Half Marathon": 21.097,
     "10km": 10.000,
     "5km": 5.000,
-    "Custom": 0.,
-    None: 0.,
+    "Custom": 0.0,
+    None: 0.0,
 }
 
 
@@ -23,50 +22,96 @@ def get_pace(dist, time):
     pace_min, pace_sec = pace // 60, pace % 60
     return pace_min, pace_sec
 
-system = st.sidebar.radio("Unit System", ('Metric', 'Imperial'), index=0)
 
-dist_unit = 'kilometers'
+system = st.sidebar.radio("Unit System", ("Metric", "Imperial"), index=0)
+
+dist_unit = "kilometers"
 if system is not None:
-    st.session_state['system'] = system
-    if st.session_state['system'] == 'Imperial':
-        dist_unit = 'miles'
+    st.session_state["system"] = system
+    if st.session_state["system"] == "Imperial":
+        dist_unit = "miles"
 
-st.header('VDOT Calculator')
+st.header("VDOT Calculator")
 
-input_method = st.radio('Choose Inputs', ('Distance & Time', 'Distance & Pace', 'Time & Pace'), horizontal=True)
-pace_disabled = input_method == 'Distance & Time'
-time_disabled = input_method == 'Distance & Pace'
-distance_disabled = input_method == 'Time & Pace'
+input_method = st.radio(
+    "Choose Inputs",
+    ("Distance & Time", "Distance & Pace", "Time & Pace"),
+    horizontal=True,
+)
+pace_disabled = input_method == "Distance & Time"
+time_disabled = input_method == "Distance & Pace"
+distance_disabled = input_method == "Time & Pace"
 
 
-st.subheader('Distance')
+st.subheader("Distance")
 distance_selection_col, distance_input_col = st.columns(2)
-default_distance: str | None = distance_selection_col.selectbox("Select distance", list(DISTANCES.keys()), key="pace_tab_distance_col_default_distance", disabled=distance_disabled)
+default_distance: str | None = distance_selection_col.selectbox(
+    "Select distance",
+    list(DISTANCES.keys()),
+    key="pace_tab_distance_col_default_distance",
+    disabled=distance_disabled,
+)
 distance = DISTANCES[default_distance]
 
-if st.session_state['system'] == 'Imperial':
+if st.session_state["system"] == "Imperial":
     distance /= 1.609
 
-raw_distance = distance_input_col.number_input("Write your distance here", 0.0, value=distance, key="pace_tab_distance_col_distance", disabled=distance_disabled)
+raw_distance = distance_input_col.number_input(
+    "Write your distance here",
+    0.0,
+    value=distance,
+    key="pace_tab_distance_col_distance",
+    disabled=distance_disabled,
+)
 
-st.subheader('Time')
+st.subheader("Time")
 
 pace_tab_time_col1, pace_tab_time_col2, pace_tab_time_col3 = st.columns(3)
-h = pace_tab_time_col1.number_input("Hours", value=0, min_value=0, key="pace_tab_time_col_hours", disabled=time_disabled)
-m = pace_tab_time_col2.number_input("Minutes", value=0, min_value=0, max_value=59, key="pace_tab_time_col_minutes", disabled=time_disabled)
-s = pace_tab_time_col3.number_input("Seconds", value=0, min_value=0, max_value=59, key="pace_tab_time_col_seconds", disabled=time_disabled)
+h = pace_tab_time_col1.number_input(
+    "Hours", value=0, min_value=0, key="pace_tab_time_col_hours", disabled=time_disabled
+)
+m = pace_tab_time_col2.number_input(
+    "Minutes",
+    value=0,
+    min_value=0,
+    max_value=59,
+    key="pace_tab_time_col_minutes",
+    disabled=time_disabled,
+)
+s = pace_tab_time_col3.number_input(
+    "Seconds",
+    value=0,
+    min_value=0,
+    max_value=59,
+    key="pace_tab_time_col_seconds",
+    disabled=time_disabled,
+)
 time = h * 60 + m + s / 60
 
-st.subheader('Pace')
+st.subheader("Pace")
 distance_tab_pace_col1, distance_tab_pace_col2 = st.columns(2)
-pace_min = distance_tab_pace_col1.number_input("Minutes", value=0, min_value=0, max_value=59, key="distance_tab_pace_col_minutes", disabled=pace_disabled)
-pace_sec = distance_tab_pace_col2.number_input("Seconds", value=0, min_value=0, max_value=59, key="distance_tab_pace_col_seconds", disabled=pace_disabled)
+pace_min = distance_tab_pace_col1.number_input(
+    "Minutes",
+    value=0,
+    min_value=0,
+    max_value=59,
+    key="distance_tab_pace_col_minutes",
+    disabled=pace_disabled,
+)
+pace_sec = distance_tab_pace_col2.number_input(
+    "Seconds",
+    value=0,
+    min_value=0,
+    max_value=59,
+    key="distance_tab_pace_col_seconds",
+    disabled=pace_disabled,
+)
 
-if input_method == 'Distance & Time':
+if input_method == "Distance & Time":
     pace_min, pace_sec = get_pace(raw_distance, time)
-elif input_method == 'Distance & Pace':
+elif input_method == "Distance & Pace":
     time = raw_distance * (pace_min + pace_sec / 60)
-elif input_method == 'Time & Pace':
+elif input_method == "Time & Pace":
     raw_distance = time / (pace_min + pace_sec / 60)
 
 try:
@@ -77,32 +122,45 @@ except ZeroDivisionError:
 st.title(f"VDOT: {vdot:.2f}")
 # print(raw_distance, time, pace_min, pace_sec)
 
-equivalent_tab, training_tab = st.tabs(['Equivalent Times', 'Training Paces'])
+equivalent_tab, training_tab = st.tabs(["Equivalent Times", "Training Paces"])
 
 with equivalent_tab:
-    df = pd.read_csv('data/interim/vdot-to-race-paces-coefficients.csv')
+    df = pd.read_csv("data/interim/vdot-to-race-paces-coefficients.csv")
 
-    df['Time'] = df.Coef1 * vdot + df.Coef2 * vdot ** 2 + df.Coef3 * vdot ** 3 + df.Coef4 * vdot ** 4 + df.Coef5 * vdot ** 5 + df.Intercept
+    df["Time"] = (
+        df.Coef1 * vdot
+        + df.Coef2 * vdot**2
+        + df.Coef3 * vdot**3
+        + df.Coef4 * vdot**4
+        + df.Coef5 * vdot**5
+        + df.Intercept
+    )
 
-    df = df.sort_values(by='Time', ascending=False)
+    df = df.sort_values(by="Time", ascending=False)
     df.Time = df.Time.apply(num_time_to_str_time)
 
-    df = df.set_index('Distance')
+    df = df.set_index("Distance")
 
-    st.dataframe(df[['Time']].T)
+    st.dataframe(df[["Time"]].T)
 
 with training_tab:
-    df = pd.read_csv('data/interim/vdot-to-training-paces-coefficients.csv')
+    df = pd.read_csv("data/interim/vdot-to-training-paces-coefficients.csv")
 
-    df['Pace'] = df.Coef1 * vdot + df.Coef2 * vdot ** 2 + df.Coef3 * vdot ** 3 + df.Coef4 * vdot ** 4 + df.Coef5 * vdot ** 5 + df.Intercept
+    df["Pace"] = (
+        df.Coef1 * vdot
+        + df.Coef2 * vdot**2
+        + df.Coef3 * vdot**3
+        + df.Coef4 * vdot**4
+        + df.Coef5 * vdot**5
+        + df.Intercept
+    )
 
-    if st.session_state['system'] == 'Imperial':
-        df['Pace'] *= 1.609
+    if st.session_state["system"] == "Imperial":
+        df["Pace"] *= 1.609
 
-    df = df.sort_values(by='Pace', ascending=False)
+    df = df.sort_values(by="Pace", ascending=False)
     df.Pace = df.Pace.apply(num_time_to_str_time)
 
-    df = df.set_index('Distance')
+    df = df.set_index("Distance")
 
-    st.dataframe(df[['Pace']].T)
-
+    st.dataframe(df[["Pace"]].T)
