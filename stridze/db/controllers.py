af5e2d320bdaae44efde968965cda05047781b13
Julien Roullé <ju.roulle@gmail.com>
2023-09-26 11:17:22 +0200

    feat: changed db size calc and add clear table method

--- stridze/db/controllers.py
+++ stridze/db/controllers.py
@@ -1,24 +1,38 @@
-from sqlalchemy import text
+from sqlalchemy import MetaData
 from sqlalchemy.orm import Session
-import typing as t
 
-from .models.user import User
 from .models.activity import Activity
-
-from .schemas.user import UserBase
+from .models.user import User
 from .schemas.activity import ActivityBase
+from .schemas.user import UserBase
+
 
 def get_db_size(db: Session) -> None:
-    query = text("SELECT SUM(data_length + index_length) / 1024 / 1024 AS size_mb FROM information_schema.tables WHERE table_schema = DATABASE();")
+    query = "SELECT pg_size_pretty(pg_database_size(current_database())) AS size;"
     result = db.execute(query)
-    size_mb = result.scalar()
 
-    print(f"The size of the database is approximately {size_mb} MB.")
+    # Fetch the result
+    size = result.fetchone()[0]
+
+    # Print the size of the database
+    print(f"The size of the database is: {size}")
+
+
+def clear_all_tables(db: Session):
+    metadata = MetaData(bind=db.bind)
+    metadata.reflect()
+
+    for table in reversed(metadata.sorted_tables):
+        table.drop(db.bind)
+
+    db.commit()
+
 
 def get_user(db: Session, user_email: str) -> User:
     user = db.query(User).filter(User.email == user_email).first()
     # if not user: return
     return user
+
 
 def create_user(db: Session, user: UserBase):
     db_user = User(**user.__dict__)
@@ -26,6 +40,7 @@ def create_user(db: Session, user: UserBase):
     db.commit()
     db.refresh(db_user)
     return db_user
+
 
 def create_activity(db: Session, activity: ActivityBase) -> Activity:
     db_element = Activity(**activity.__dict__)
