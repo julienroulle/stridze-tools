301af6147397ea38a04511f522217131c651f3f8
Julien Roullé <ju.roulle@gmail.com>
2023-10-17 11:24:43 +0200

    refactor: remove vdot from user

--- stridze/db/models/user.py
+++ stridze/db/models/user.py
@@ -3,6 +3,7 @@ from sqlalchemy.orm import relationship
 
 from .. import Base
 
+
 class User(Base):
     __tablename__ = "users"
 
@@ -11,6 +12,5 @@ class User(Base):
     last_name = Column(String(32))
     email = Column(String(32), unique=True, index=True, nullable=False)
     password = Column(String(32))
-    vdot = Column(Integer)
 
-    activities = relationship('Activity', back_populates='user')
+    activities = relationship("Activity", back_populates="user")
